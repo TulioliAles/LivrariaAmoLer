0ca9668418ec05d46c77602b6d4431a8f7f82b97
TulioliAles <desmecad@hotmail.com>
2020-10-30 18:18:31 -0300

    Criada a View para cadastrar um livro

---
+++ LivrariaAmoLer/Controllers/LivroController.cs
@@ -0,0 +1,35 @@
+﻿using LivrariaAmoLer.Context;
+using LivrariaAmoLer.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LivrariaAmoLer.Controllers
+{
+    [RoutePrefix("livros")]
+    public class LivroController : Controller
+    {
+        LivrariaAmoLerDataContext _db = new LivrariaAmoLerDataContext();
+
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            var categorias = _db.Categorias.ToList();
+            var model = new CreateLivroViewModel
+            {
+                Titulo = "",
+                ISBN = "",
+                CategoriaId = 0,
+                CategoriaOtions = new SelectList(categorias, "Id", "Nome")
+            };
+
+            return View(model);
+        }
+
+        [Route("criar")]
+        [HttpPost]
+        public ActionResult Create(CreateLivroViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ LivrariaAmoLer/ViewModels/CreateLivroViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace LivrariaAmoLer.ViewModels
+{
+    public class CreateLivroViewModel
+    {
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Nome do livro")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Categoria")]
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Data de lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+
+        public SelectList CategoriaOtions { get; set; }
+    }
+}
